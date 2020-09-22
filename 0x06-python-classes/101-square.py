4e0333d6bd8e93dcc7c0a78475ee774d0d93229f
matcls <manueltorresvesga@gmail.com>
2020-09-22 16:54:07 -0500

    typo

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -67,7 +67,7 @@ class Square:
                 not isinstance(value[1], int) or
                 (value[0] < 0 or value[1] < 0)
         ):
-            raise TypeError("position must be a tuple of 2 positive integer")
+            raise TypeError("position must be a tuple of 2 positive integers")
         else:
             self.__position = value
 
