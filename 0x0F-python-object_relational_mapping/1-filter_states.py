989c2b2e2fd4f50a86cfc9e2c1b435544806b3d8
matcls <manueltorresvesga@gmail.com>
2020-12-12 16:39:55 -0500

    Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
    Take 3 arguments: mysql username, mysql password and database name (no argument validation needed)
    Use the module MySQLdb (import MySQLdb)
    Connect to a MySQL server running on localhost at port 3306
    Results must be sorted in ascending order by states.id
    Only be executed when imported

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Lists all states with a name starting with N (upper N)
+   from the database hbtn_0e_0_usa.
+"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+
+    import MySQLdb
+
+    if len(argv) == 4:
+        connect = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            charset="utf8",
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3])
+
+        cursor = connect.cursor()
+        cursor.execute("SELECT * FROM states WHERE name LIKE BINARY \
+        'N%' ORDER BY states.id ASC")
+        query_rows = [print(state) for state in cursor.fetchall()]
+        cursor.close()
+        connect.close()
+    else:
+        print("Usage: mysql username, mysql password, database name")
