b0bb038a30070d213bcaee18c5b6be30c8e30497
matcls <manueltorresvesga@gmail.com>
2020-12-12 16:54:54 -0500

    Displays all values in the states table where name matches the argument
    Take 4 arguments: mysql username, mysql password, database name and state name searched
    (no argument validation needed)
    Use the module MySQLdb (import MySQLdb)
    Connect to a MySQL server running on localhost at port 3306
    Use format to create the SQL query with the user input
    Results must be sorted in ascending order by states.id
    Not executed when imported

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument
+"""
+if __name__ == "__main__":
+    from sys import argv
+
+    import MySQLdb
+
+    if len(argv) == 5:
+        connect = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            charset="utf8",
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3])
+
+        cursor = connect.cursor()
+        cursor.execute("SELECT * FROM states WHERE name LIKE BINARY \
+        '{}' ORDER BY states.id ASC".format(argv[4]))
+        query_rows = [print(state) for state in cursor.fetchall()]
+        cursor.close()
+        connect.close()
+    else:
+        print("Usage: mysql username, mysql password, database name")
