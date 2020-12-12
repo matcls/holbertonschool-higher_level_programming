71112bd6ed16ea12b95fbc89def06b647b4125a8
matcls <manueltorresvesga@gmail.com>
2020-12-12 14:35:05 -0500

    Show usage message if 3 arguments are not given

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,15 +6,18 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    connect = MySQLdb.connect(
-        host="localhost",
-        port=3306,
-        charset="utf8",
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3])
-    cur = connect.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
-    query_rows = [print(state) for state in cur.fetchall()]
-    cur.close()
-    connect.close()
+    if len(argv) == 4:
+        connect = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            charset="utf8",
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3])
+        cur = connect.cursor()
+        cur.execute("SELECT * FROM states ORDER BY states.id ASC")
+        query_rows = [print(state) for state in cur.fetchall()]
+        cur.close()
+        connect.close()
+    else:
+        print("Usage: mysql username, mysql password, database name")
