5300236c3fd1a555a81e29928dd3ebce6f86ef03
matcls <manueltorresvesga@gmail.com>
2020-12-12 16:39:37 -0500

    Import only if executed as main program

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """Lists all states from the database hbtn_0e_0_usa:"""
 
-from sys import argv
-
-import MySQLdb
-
 if __name__ == "__main__":
+    from sys import argv
+
+    import MySQLdb
+
     if len(argv) == 4:
         connect = MySQLdb.connect(
             host="localhost",
