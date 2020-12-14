536b7a5e18d8d26567293a611f0bd646dc48e806
matcls <manueltorresvesga@gmail.com>
2020-12-13 23:20:20 -0500

    Add missing import

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -4,20 +4,22 @@ Create the State California with the City San Francisco
 from the database hbtn_0e_100_usa.
 """
 
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker, relationship
-from sys import argv
-from relationship_state import State, Base, City
+if __name__ == "__main__":
+    from sqlalchemy import (create_engine)
+    from sqlalchemy.orm import sessionmaker
+    from sys import argv
+    from relationship_state import State, Base
+    from relationship_city import City
 
-engine = create_engine(
-    'mysql+mysqldb://{}:{}@localhost:3306/{}'
-    .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-Base.metadata.create_all(engine)
-Session = sessionmaker(bind=engine)
-session = Session()
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-session.add(City(name="San Francisco", state=State(name="California")))
-session.commit()
-session.close()
+    session.add(City(name="San Francisco", state=State(name="California")))
+    session.commit()
+    session.close()
 
