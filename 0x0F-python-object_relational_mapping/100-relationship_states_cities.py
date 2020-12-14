74d0adb8a39ba76c3c171d7f5a653706f07e7620
matcls <manueltorresvesga@gmail.com>
2020-12-13 23:02:25 -0500

    Remove argument count validation

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -4,22 +4,19 @@ Create the State California with the City San Francisco
 from the database hbtn_0e_100_usa.
 """
 
-if __name__ == "__main__":
-    from sqlalchemy import (create_engine)
-    from sqlalchemy.orm import sessionmaker, relationship
-    from sys import argv
-    from relationship_state import State, Base, City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker, relationship
+from sys import argv
+from relationship_state import State, Base, City
 
-    if len(argv) == 4:
-        engine = create_engine(
-            'mysql+mysqldb://{}:{}@localhost:3306/{}'
-            .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
+if len(argv) == 4:
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost:3306/{}'
+        .format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-        session.add(City(name="San Francisco", state=State(name="California")))
-        session.commit()
-    else:
-        print("Usage: mysql_username mysql_password database_name")
+    session.add(City(name="San Francisco", state=State(name="California")))
+    session.commit()
