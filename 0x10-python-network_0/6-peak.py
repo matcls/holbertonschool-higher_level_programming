a1a6801dad72052fad41f62038f826932dec584b
matcls <manueltorresvesga@gmail.com>
2020-12-18 20:53:26 -0500

    Let python do the job

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,22 +4,8 @@
 
 def find_peak(list_of_integers):
     """Find a peak in a list of unsorted integers"""
-    size = len(list_of_integers)
-    if size < 3:
-        return None
-    peak = peak_finder(list_of_integers, 0, size - 1)
-    return peak
-
-
-def peak_finder(list_of_integers, left, rigth):
-    """Find the peak in a list of unsorted list of integers"""
-    mid = (rigth - left) // 2 + left
-    integers = list_of_integers
-
-    if mid + 1 >= rigth + 1 or integers[mid + 1] <= integers[mid]:
-        if mid - 1 < 0 or integers[mid - 1] <= integers[mid]:
-            return integers[mid]
-        else:
-            return find_peak(integers, 0, mid)
+    if list_of_integers:
+        list_of_integers.sort()
+        return list_of_integers[-1]
     else:
-        return peak_finder(integers, mid + 1, rigth)
+        return None
