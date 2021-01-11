a4f5de223b8ae80878a6b49a49bb273e4a8e2ba3
matcls <manueltorresvesga@gmail.com>
2021-01-11 12:20:23 -0500

    Order imports alphabetically

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,9 +2,10 @@
 """Takes in a URL, sends a request to the URL and displays the value of the
 X-Request-Id variable found in the header of the response."""
 
+
 if __name__ == "__main__":
-    from urllib import request
     from sys import argv
+    from urllib import request
 
     url = argv[1]
     with request.urlopen(url) as response:
