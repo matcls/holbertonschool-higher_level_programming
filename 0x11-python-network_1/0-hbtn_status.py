89c7910d5da0cda7842fd9e18808baae8ec90f9b
matcls <manueltorresvesga@gmail.com>
2021-01-11 11:51:36 -0500

    add Python script that fetches https://intranet.hbtn.io/status
    Use the package urllib
    Not allowed to import any packages other than urllib
    You must use a with statement

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Fetches a URL"""
+if __name__ == "__main__":
+    from urllib import request
+    url = 'https://intranet.hbtn.io/status'
+    with request.urlopen(url) as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type:', type(html))
+        print('\t- content:', html)
+        print('\t- utf8 content:', html.decode('utf-8'))
