ce3a5d97b07a0679d98bce9f3800753dd7ba89e7
matcls <manueltorresvesga@gmail.com>
2021-01-11 14:02:29 -0500

    Check raised error for status

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     try:
         url = argv[1]
         response = requests.get(url)
+        response.raise_for_status()
         print(response.text)
     except requests.exceptions.RequestException as err:
         print('Error code:', err.response.status_code)
